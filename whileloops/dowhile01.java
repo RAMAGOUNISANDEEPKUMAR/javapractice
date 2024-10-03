4f235890ea8d6974c1f384a8ccd96bb2c32a5e3b
RAMAGOUNISANDEEPKUMAR <sandeepkumar16058@gmail.com>
2024-10-03 10:59:51 +0530

    new

---
+++ whileloops/dowhile01.java
@@ -0,0 +1,11 @@
+class dowhile01
+{
+    public static void main(String[] args)
+    {
+        int i =1;
+        do {
+            System.out.println("iteration: "+ i);
+            i++;
+        }while(i <= 5);
+    }
+}
---
+++ whileloops/infinitedowhile.java
@@ -0,0 +1,9 @@
+class infintedowhile
+{
+    public static void main(String[] args)
+    {
+        do{
+            System.out.println("infinitive do while loop");
+        }while(true);
+    }
+}
